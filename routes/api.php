ebcbd5743ccd0f3ab5da4d9f96783d4ee14243ac
alxjr.wav <20245387@esen.edu.sv>
2026-03-23 17:16:19 -0600

    feat: Introduce user API routes aND UserController

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 //Routes that do not require Authentication
 Route::post("/v1/auth/login", [AuthController::class,"login"]);
@@ -9,6 +10,17 @@ Route::post("/v1/auth/login", [AuthController::class,"login"]);
 Route::middleware('auth:sanctum')->prefix('/v1')->group(function () {
 
     //Routes that require Authentication go here
+    // TODO: CRUD users
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+    // TODO: CRUD Schedule para médicos
+    // For updating, the doctor must be informed that previous appointments can not be changed, but future ones could be
+
+    // TODO: CRUD Appointment for pacients
     
     //Authtentication routes
     Route::get('/auth/profile', [AuthController::class, 'profile']);
