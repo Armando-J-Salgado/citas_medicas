99904efd3a5df7bbe7d58642dd77effee96024cb
Armando Salgado <20245138@esen.edu.sv>
2026-03-23 11:40:22 -0600

    Authentication through token **feature**: Token authorization implemented. Protected Api routes ready.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Routes that do not require Authentication
+Route::post("/v1/auth/login", [AuthController::class,"login"]);
+
+Route::middleware('auth:sanctum')->prefix('/v1')->group(function () {
+
+    //Routes that require Authentication go here
+    
+    //Authtentication routes
+    Route::get('/auth/profile', [AuthController::class, 'profile']);
+    Route::post('/auth/logout', [AuthController::class,'logout']);
+
+});
