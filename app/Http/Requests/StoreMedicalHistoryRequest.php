6805b3930919147208cf7b214ed7266223cf3ad8
alxjr.wav <20245387@esen.edu.sv>
2026-03-23 23:18:15 -0600

    fix: Add request validation classes for every model

--- app/Http/Requests/StoreMedicalHistoryRequest.php
+++ app/Http/Requests/StoreMedicalHistoryRequest.php
@@ -12,7 +12,7 @@ class StoreMedicalHistoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/StoreScheduleRequest.php
+++ app/Http/Requests/StoreScheduleRequest.php
@@ -12,7 +12,7 @@ class StoreScheduleRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
