8dff0d7644f40a004c16b5d78dc26d96bfa4aed9
Armando Salgado <20245138@esen.edu.sv>
2026-03-24 11:22:36 -0600

    fix: validation in Medical History

--- app/Filament/Resources/MedicalHistories/Pages/ManageMedicalHistories.php
+++ app/Filament/Resources/MedicalHistories/Pages/ManageMedicalHistories.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\MedicalHistories\Pages;
 
 use App\Filament\Resources\MedicalHistories\MedicalHistoryResource;
+use App\Models\Pacient;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ManageRecords;
+use Illuminate\Validation\ValidationException;
+use function PHPUnit\Framework\isNan;
 
 class ManageMedicalHistories extends ManageRecords
 {
@@ -13,7 +16,24 @@ class ManageMedicalHistories extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()
+                ->mutateFormDataUsing(function (array $data): array {
+                $this->validateMedicalHistory($data);
+                return $data;
+            }),
         ];
+    }
+
+    private function validateMedicalHistory(array $data): void
+    {
+        $pacientId = $data["pacient_id"] ?? null;
+
+        $pacient = Pacient::find($pacientId);
+
+        if ($pacient && $pacient->MedicalHistory) {
+            throw ValidationException::withMessages([
+                'user' => 'The pacient does not have available schedules at this time or already has an appointment.',
+            ]);
+        }
     }
 }
